e6e9e1423e091290198c085ed905f5d415bf526b
Chan Yik Wai <yikwai1998@gmail.com>
2023-08-28 15:36:32 +0800

    enhance_test

--- ex03/srcs/main.cpp
+++ ex03/srcs/main.cpp
@@ -5,35 +5,35 @@
 //reference needs to initialized when the constructor is called; it not reasonable cauz human doesnt always armed.
 // pointer can change the address while reference supposed to be const.
 
-// int main()
-// {
-// 	//first codes // ref
-// 	{
-// 		Weapon club = Weapon("crude spiked club");
+int test(void)
+{
+	//first codes // ref
+	{
+		Weapon club = Weapon("crude spiked club");
 
-// 		HumanA bob("Bob", club);
-// 		bob.attack();
-// 		club.setType("some other type of club");//Weapon member
-// 		club.setType("");
-// 		bob.attack();
-// 	}
-// 	//second codes //ptr
-// 	{
-// 		Weapon club = Weapon("crude spiked club");
-// 		Weapon un = Weapon("");
+		HumanA bob("Bob", club);
+		bob.attack();
+		club.setType("some other type of club");//Weapon member
+		club.setType("");
+		bob.attack();
+	}
+	//second codes //ptr
+	{
+		Weapon club = Weapon("crude spiked club");
+		Weapon un = Weapon("");
 
-// 		HumanB jim("Jim");
-// 		jim.setWeapon(club); //classB member->picking up the weapon slot
-// 		jim.attack();
-// 		club.setType("some other type of club");//Weapon member
-// 		jim.attack();
-// 		jim.setWeapon(un);
+		HumanB jim("Jim");
+		jim.setWeapon(club); //classB member->picking up the weapon slot
+		jim.attack();
+		club.setType("some other type of club");//Weapon member
+		jim.attack();
+		jim.setWeapon(un);
 		
-// 		jim.attack();
-// 	}
-// 	// system ("leaks -q run");
-// 	return 0;
-// }
+		jim.attack();
+	}
+	// system ("leaks -q run");
+	return 0;
+}
 
 int main()
 {
@@ -52,6 +52,8 @@ int main()
 		club.setType("some other type of club");
 		jim.attack();
 	}
+	std::cin.get();
+	test();
 	// system ("leaks -q run");
 	return (0);
 }
